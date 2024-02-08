b47d647abd4326c207cb1b8ba3656612a9b977d2
Bryce Doperalski <brycedoperalski@gmail.com>
2024-02-08 14:58:06 -0600

    Update App.java

--- src/fortuneteller/App.java
+++ src/fortuneteller/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Fortune Teller");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
