fcc5f3a9d23753772bee47adf6c13ae38326b98f
Bryce Doperalski <brycedoperalski@gmail.com>
2024-02-19 15:10:33 -0600

    started an info screen and switching between it and the start

--- src/fortuneteller/AppController.java
+++ src/fortuneteller/AppController.java
@@ -1,9 +1,16 @@
 package fortuneteller;
 
+import java.io.IOException;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class AppController {
 
@@ -22,8 +29,16 @@ public class AppController {
     }
 
     @FXML
-    void infoButtonPressed(ActionEvent event) {
+    void infoButtonPressed(ActionEvent event) throws IOException {
+        Stage stage;
+        Scene scene;
+        Parent root;
 
+        root = FXMLLoader.load(getClass().getResource("info.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
